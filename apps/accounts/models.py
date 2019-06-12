bb1dae93b0fffca32902f0c5bee9a5be1fc3d364
Tushant Khatiwada <programmertushant@gmail.com>
2019-06-12 20:58:26 +0545

    trying to fix merge issue to dev as all the changes were not merged because of some branch merging to master and some to dev

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -27,7 +27,7 @@ class User(AbstractUser):
 
 class Profile(TimeStampModels):
     '''
-        Profile modal common to all kinds of users
+    Profile modal common to all kinds of users
     '''
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100, blank=True, null=True)
